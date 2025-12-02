504bef28cce2e48ad52d01f36efce831e3ff2ef0
cmurga95 <125426661+cmurga95@users.noreply.github.com>
2025-12-02 15:01:16 +0000

    Deploying to gh-pages from @ cmurga95/cmurga95.github.io@417e4c705d6c9e9d07f21bc348bd3bfc269fcaba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,12 +451,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-confie-collections-report",
-          title: 'Confie Collections Report',
-          description: "Tableau Report with multiple views and filters",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -496,6 +491,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/confie%20collections%20report/";
             },},{
         id: 'social-cv',
         title: 'CV',
