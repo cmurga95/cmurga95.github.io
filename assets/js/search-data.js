1c58a5ca16ca95b5bdef5ef0146d513dc24427c4
cmurga95 <125426661+cmurga95@users.noreply.github.com>
2025-12-03 20:39:28 +0000

    Deploying to gh-pages from @ cmurga95/cmurga95.github.io@5303ee4871d228a0cbd4943692d4a36723685e4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,42 +452,7 @@ ninja.data = [{
           section: "News",},{id: "news-started-master-of-science-of-analytics-officially-i-transferred-the-credits-from-cse6040-too",
           title: 'Started Master of Science of Analytics officially. I transferred the credits from CSE6040...',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-attendance-and-leads-report",
+          section: "News",},{id: "projects-attendance-and-leads-report",
           title: 'Attendance and Leads Report',
           description: "BPO division wanted a dashboard where they could visualize a summary of all KPI of all",
           section: "Projects",handler: () => {
@@ -502,6 +467,11 @@ ninja.data = [{
           description: "Tableau Report with multiple views and filters",
           section: "Projects",handler: () => {
               window.location.href = "/projects/confie_collections_report/";
+            },},{id: "projects-mea-analysis",
+          title: 'MEA Analysis',
+          description: "Multi Electrode Array Feature Analysis and Visualization",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/unc_mea_analysis/";
             },},{
         id: 'social-cv',
         title: 'CV',
